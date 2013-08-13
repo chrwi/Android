b2cde468b514e5f0286b62e2f523d0677bb12531
Christian Wilhelmsen <christian.wilhelmsen87@gmail.com>
2013-08-13 23:00:25 +0200

    added new intent

--- HelloWorldProject/HelloWorld/build/source/r/debug/com/wilhelmsen/helloworld/R.java
+++ HelloWorldProject/HelloWorld/build/source/r/debug/com/wilhelmsen/helloworld/R.java
@@ -11,8 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        /**  From: file:/C:/Users/chw1/AndroidStudioProjects/HelloWorldProject/HelloWorld/src/main/res/values/dimens.xml 
- From: file:/C:/Users/chw1/AndroidStudioProjects/HelloWorldProject/HelloWorld/src/main/res/values-sw720dp-land/dimens.xml 
+        /**  From: file:/D:/Prosjekter/Android/Android/HelloWorldProject/HelloWorld/src/main/res/values/dimens.xml 
+ From: file:/D:/Prosjekter/Android/Android/HelloWorldProject/HelloWorld/src/main/res/values-sw720dp-land/dimens.xml 
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
@@ -21,10 +21,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080007;
         public static final int button=0x7f080001;
         public static final int checkBox=0x7f080002;
         public static final int checkBox2=0x7f080003;
+        public static final int messageTextBox=0x7f080004;
+        public static final int statusText=0x7f080006;
+        public static final int textButton=0x7f080005;
         public static final int textView=0x7f080000;
     }
     public static final class layout {
@@ -34,7 +37,7 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        /**  From: file:/C:/Users/chw1/AndroidStudioProjects/HelloWorldProject/HelloWorld/src/main/res/values/strings.xml 
+        /**  From: file:/D:/Prosjekter/Android/Android/HelloWorldProject/HelloWorld/src/main/res/values/strings.xml 
          */
         public static final int action_settings=0x7f050000;
         public static final int app_name=0x7f050001;
@@ -43,15 +46,15 @@ public final class R {
         public static final int hello_world=0x7f050004;
     }
     public static final class style {
-        /**  From: file:/C:/Users/chw1/AndroidStudioProjects/HelloWorldProject/HelloWorld/src/main/res/values/styles.xml 
+        /**  From: file:/D:/Prosjekter/Android/Android/HelloWorldProject/HelloWorld/src/main/res/values/styles.xml 
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
- From: file:/C:/Users/chw1/AndroidStudioProjects/HelloWorldProject/HelloWorld/src/main/res/values-v11/styles.xml 
+ From: file:/D:/Prosjekter/Android/Android/HelloWorldProject/HelloWorld/src/main/res/values-v11/styles.xml 
  API 11 theme customizations can go here. 
- From: file:/C:/Users/chw1/AndroidStudioProjects/HelloWorldProject/HelloWorld/src/main/res/values-v14/styles.xml 
+ From: file:/D:/Prosjekter/Android/Android/HelloWorldProject/HelloWorld/src/main/res/values-v14/styles.xml 
  API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f060000;
